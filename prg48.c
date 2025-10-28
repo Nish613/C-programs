1d995cea346ebb1cac1ec5f3c962a99fae9bf94c
Nishtha Gupta <nishtha2711@gmail.com>
2025-10-28 12:30:43 +0530

    prg48 modified

--- prg48.c
+++ prg48.c
@@ -1,4 +1,8 @@
-// BST
+/*Write a program to create a BST and perform inorder, preorder, and postorder 
+traversals. 
+2. Write a function to search an element in a BST. 
+3. Write a function to delete a node from a BST. 
+4. Write a function to find the minimum and maximum elements in a BST. */
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -18,7 +22,51 @@ struct Node* createNode(int data)
     return newNode;
 }
 
-struct Node* createBST()
+struct Node* createBST(struct Node* root,int data)
 {
-    
+    if(root==NULL)
+    createNode(data);
+
+    if(data<root->data)
+    root->left = createBST(root->left,data);
+
+    else if(data>root->data)
+    root->right = createBST(root->right,data);
+
+    return root;
 }
+
+void inorder(struct Node* root)
+{
+    if(root==NULL)
+    return;
+
+    inorder(root->left);
+    printf("%d\t",root->data);
+    inorder(root->right);
+}
+
+void preorder(struct Node* root)
+{
+    if(root==NULL)
+    return;
+
+    printf("%d\t",root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void postorder(struct Node* root)
+{
+    if(root==NULL)
+    return;
+
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d\t",root->data);
+}
+
+
+
+
+
